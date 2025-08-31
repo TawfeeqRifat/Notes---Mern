2d4c858f7a2f46fa0aa793bbdfc820db70a3d2bc
Tawfeeq Rifat J <tawfeeqrifat@gmail.com>
2025-08-31 09:04:12 +0530

    Delete and Update Notes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react'
 import NavBar from '../components/NavBar'
 import RateLimitedUi from '../components/rateLimitedUi'
+import NotesNotFound from '../components/NotesNotFound'
 import {useState} from 'react'
 import axios from "axios"
 import toast from "react-hot-toast";
 import NoteCard from "../components/NoteCard";
+import api from '../lib/axios'
 const homepage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
   const [notes,setNotes] = useState([])
@@ -13,7 +15,7 @@ const homepage = () => {
   useEffect(() => {
     const fetchNotes = async () => {
       try{
-        const res = await axios.get("http://localhost:5001/api/notes")
+        const res = await api.get("/notes")
         console.log(res.data)
         setNotes(res.data)
         setIsRateLimited(false)
@@ -41,10 +43,12 @@ const homepage = () => {
 
       <div className='max-w-7xl mx-auto p-4 mt-6'>
         {loading && <div className="text-center text-primary py-10">Loading Notes... </div>}
+
+        {notes.length === 0&& !isRateLimited && <NotesNotFound />}
         { notes.length > 0 && !isRateLimited && (
           <div className= "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"> 
             {notes.map((note) => (
-              <NoteCard key={note._id} note={note} /> 
+              <NoteCard key={note._id} note={note} setNotes={setNotes}/> 
             ))}
           </div>
         )}
